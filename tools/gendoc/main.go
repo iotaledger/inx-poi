a15da3b84416ec064348d2c103577d8c11ca14cc
muXxer <git@muxxer.de>
2023-08-30 21:49:22 +0200

    Update go to version 1.21 (#61)
    * Update go to version 1.21
    
    * Fix gendoc
    
    * Disable depguard
    
    * Update workflows

--- tools/gendoc/main.go
+++ tools/gendoc/main.go
@@ -70,7 +70,7 @@ func main() {
 	createMarkdownFile(
 		application,
 		"configuration_header.md",
-		"../../documentation/docs/configuration.md",
+		"../../configuration.md",
 		ignoreFlags,
 		replaceTopicNames,
 	)
